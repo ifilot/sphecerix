4b02ebc185deee2906ce237b5d0529cbc23d2a4b
ifilot <ivo@ivofilot.nl>
2023-05-26 20:33:27 +0200

    Initial commit CI/CD

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ifilot/sphecerix",
+    packages=[PKG],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
