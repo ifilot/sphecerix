dea5b69144fe16d25191c9592dcc2f765ca19ca9
ifilot <ivo@ivofilot.nl>
2023-05-26 20:28:51 +0200

    Initial commit CI/CD

--- sphecerix/__init__.py
+++ sphecerix/__init__.py
@@ -5,3 +5,5 @@ from .wignerd import wigner_D, wigner_d, wigner_D_axis_angle, tesseral_wigner_D_
 from .tesseral import tesseral_transformation, permutation_sh_car
 
 from .atomic_wave_functions import wfcart, wf, wffield, wffield_l
+
+from ._version import __version__
