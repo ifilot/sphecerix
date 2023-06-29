cac22f27d4a1bc157fdbc0cbf5bfa3f9516bca0c
ifilot <ivo@ivofilot.nl>
2023-06-29 09:10:53 +0200

    Including package data

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ifilot/sphecerix",
     packages=[PKG, PKG + '.charactertables'],
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
