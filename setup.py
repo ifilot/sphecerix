af5ad749f87589337ddda83caa0223ec76a82dec
ifilot <ivo@ivofilot.nl>
2023-06-29 09:06:17 +0200

    Adding character tables to setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ifilot/sphecerix",
-    packages=[PKG],
+    packages=[PKG, PKG + '.charactertables'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
